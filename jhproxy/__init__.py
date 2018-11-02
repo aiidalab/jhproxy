5c4b916859a8520fc285252050f7473a6f34b421
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2018-11-02 10:03:04 +0100

    Adding 0.1.0 version number
    Also added traitlets dependency

--- jhproxy/__init__.py
+++ jhproxy/__init__.py
@@ -1,1 +1,3 @@
 from .proxy import ProxyHandler, ProxyTokenHandler
+
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python
-
 from __future__ import absolute_import
 from setuptools import setup, find_packages
+import os
+
+def get_version():
+    """Dumb version parser"""
+    initfname = os.path.join(
+        os.path.split(os.path.realpath(__file__))[0],
+        'jhproxy',
+        '__init__.py')
+    version = None
+    with open(initfname) as f:
+        for l in f:
+            if '__version__' in l:
+                version = l.partition('#')[0].split('=')[1].strip().replace('"', '').replace("'", '')
+                break
+
+    if version is None:
+        raise RuntimeError("Unable to get package version number")
+
+    if '.' not in version:
+        raise RuntimeError("No dots find in version")
+
+    return version
+
 
 if __name__ == '__main__':
-    # Provide static information in setup.json
-    # such that it can be discovered automatically
     setup(
         name="jhproxy",
         author="Giovanni Pizzi",
         packages=find_packages(),
         description=
         'A proxy for ports inside Jupyter when running with a DockerSpawner',
-        #url=
+        url="https://github.com/aiidalab/jhproxy",
         license="MIT",
         classifiers=["Programming Language :: Python"],
-        version="0.0.1",
-        install_requires=['jupyterhub', 'dockerspawner'],
+        version=get_version(),
+        install_requires=['jupyterhub', 'dockerspawner', 'traitlets'],
     )
